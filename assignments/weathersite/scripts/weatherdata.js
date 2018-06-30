f27ced90d810c3b969e2be35370332ddc7c0b406
kayrenekelley <kelleydandk@gmail.com>
2018-06-30 16:48:44 -0600

    corrections

--- assignments/weathersite/scripts/weatherdata.js
+++ assignments/weathersite/scripts/weatherdata.js
@@ -1,5 +1,5 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET','//api.openweathermap.org/data/2.5/weatherzip=55333,us&appid=7cec70771bb416e2ba95cdd1b2a79fd8&units=imperial',true);
+weatherObject.open('GET','//api.openweathermap.org/data/2.5/weatherzip=55333,us&appid=9552cb3826b9862aa95bc2bcf8fc002e&units=imperial',true);
 
 weatherObject.send();
 
@@ -9,12 +9,12 @@ weatherObject.onload = function(){
     
     document.getElementById('place').innerHTML = weatherInfo.name;
     document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
-    document.getElementById('currentDisc').innerHTML = weatherInfo.weather[1].description;
+    document.getElementById('currentDisc').innerHTML = weatherInfo.weather[0].description;
     document.getElementById('highTemp').innerHTML = weatherInfo.main.temp_max;
     document.getElementById('lowTemp').innerHTML = weatherInfo.main.temp_min;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
     
-    var iconcode = weatherInfo.weather[0].icon;
+    var iconcode = weatherInfo.weather[1].icon;
     var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     document.getElementById('weather_icon').src = icon_path;
     

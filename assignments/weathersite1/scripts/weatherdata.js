f637d7add8ef0841703de6869eddbc6016813472
kayrenekelley <kelleydandk@gmail.com>
2018-07-01 02:44:26 -0600

    corrections l 10

--- assignments/weathersite1/scripts/weatherdata.js
+++ assignments/weathersite1/scripts/weatherdata.js
@@ -1,5 +1,5 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET','https://api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=7cec70771bb416e2ba95cdd1b2a79fd8&units=imperial',true);
+weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=7cec70771bb416e2ba95cdd1b2a79fd8&units=imperial',true);
 
 weatherObject.send();
 
@@ -14,4 +14,8 @@ weatherObject.onload = function(){
     document.getElementById('lowTemp').innerHTML = weatherInfo.main.temp_min;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
     
+    
+     var iconcode = weatherInfo.weather[1].icon;
+    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
 }

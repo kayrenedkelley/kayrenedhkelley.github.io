ca6e342688bd5c6435723b69c2d07a65a5a4c9cc
kayrenekelley <kelleydandk@gmail.com>
2018-07-03 15:40:41 -0600

    weather corrections lesson 10

--- assignments/weathersite1/scripts/weatherdata.js
+++ assignments/weathersite1/scripts/weatherdata.js
@@ -1,5 +1,5 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=7cec70771bb416e2ba95cdd1b2a79fd8&units=imperial',true);
+weatherObject.open('GET','https://api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=9552cb3826b9862aa95bc2bcf8fc002e&units=imperial',true);
 
 weatherObject.send();
 
@@ -15,7 +15,13 @@ weatherObject.onload = function(){
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
     
     
-     var iconcode = weatherInfo.weather[1].icon;
+     var iconcode = weatherInfo.weather[0].icon;
     var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
     document.getElementById('weather_icon').src = icon_path;
 }
+
+
+
+
+
+//weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?zip=55333,us&appid=7cec70771bb416e2ba95cdd1b2a79fd8&units=imperial',true);
